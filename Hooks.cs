7dadc623b9260f089c3daa851eafe38cb2b9c95c
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-03-05 17:57:49 +0000

    Proper name for internal logger

--- Hooks.cs
+++ Hooks.cs
@@ -93,7 +93,7 @@ namespace Core.FsmUtil
                 catch (Exception ex)
                 {
                     logger.LogError(ex);
-                    int_Logger.Log(ex.ToString());
+                    Core.FsmUtil.Logger.Log(ex.ToString());
                 }
             }
         }
